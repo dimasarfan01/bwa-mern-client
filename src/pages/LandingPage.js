4dcb45de1545c721773c5550a274f1d4acffa2c0
dimasarfan01 <dimasarfanasyuro123@gmail.com>
2021-09-19 21:10:26 +0700

    finished component categories

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,11 +1,25 @@
+import landingPage from "json/landingPage.json";
+import Categories from "parts/Categories";
 import Header from "parts/Header";
+import Hero from "parts/Hero";
+import MostPicked from "parts/MostPicked";
 import React, { Component } from "react";
 
 export default class LandingPage extends Component {
+  constructor(props) {
+    super(props);
+    this.refMostPicked = React.createRef();
+  }
   render() {
     return (
       <>
         <Header {...this.props}></Header>
+        <Hero refMostPicked={this.refMostPicked} data={landingPage.hero} />
+        <MostPicked
+          refMostPicked={this.refMostPicked}
+          data={landingPage.mostPicked}
+        />
+        <Categories data={landingPage.categories} />
       </>
     );
   }
