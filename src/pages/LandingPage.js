bbd3caf2e24fe94ebe3dd4dc38df576743694ca7
dimasarfan01 <dimasarfanasyuro123@gmail.com>
2021-09-20 09:06:25 +0700

    added react-reveal

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,8 +1,10 @@
 import landingPage from "json/landingPage.json";
 import Categories from "parts/Categories";
+import Footer from "parts/Footer";
 import Header from "parts/Header";
 import Hero from "parts/Hero";
 import MostPicked from "parts/MostPicked";
+import Testimony from "parts/Testimony";
 import React, { Component } from "react";
 
 export default class LandingPage extends Component {
@@ -20,6 +22,8 @@ export default class LandingPage extends Component {
           data={landingPage.mostPicked}
         />
         <Categories data={landingPage.categories} />
+        <Testimony data={landingPage.testimonial} />
+        <Footer />
       </>
     );
   }
--- src/parts/Hero.js
+++ src/parts/Hero.js
@@ -1,10 +1,11 @@
-import React from "react";
-import ImageHero from "assets/images/img-hero.jpg";
-import ImageHeroFrame from "assets/images/img-hero-frame.jpg";
 import IconCities from "assets/images/icons/icon-cities.svg";
 import IconTraveler from "assets/images/icons/icon-traveler.svg";
 import IconTreasure from "assets/images/icons/icon-treasure.svg";
+import ImageHeroFrame from "assets/images/img-hero-frame.jpg";
+import ImageHero from "assets/images/img-hero.jpg";
 import Button from "elements/Button";
+import React from "react";
+import Fade from "react-reveal/Fade";
 import { numberFormat } from "utils/formatNumber";
 
 export default function Hero(props) {
@@ -28,68 +29,70 @@ export default function Hero(props) {
   };
 
   return (
-    <section className="container pt-4">
-      <div className="row align-items-center">
-        <div className="col-auto pr-5" style={{ width: 530 }}>
-          <h1 className="font-weight-bold line-height-1 mb-3">
-            Forget Busy Work, <br />
-            Start Next Vacation
-          </h1>
-          <p
-            className="mb-4 font-weight-light text-gray-500 w-75"
-            style={{ lineHeight: "170%" }}
-          >
-            We provide what you need to enjoy your holiday with family. Time to
-            make another memorable moments.
-          </p>
-          <Button
-            className="btn px-5"
-            hasShadow
-            isPrimary
-            onClick={showMostPicked}
-          >
-            Show Me Now
-          </Button>
-          <div className="row" style={{ marginTop: 80 }}>
-            <IconHero
-              imageSrc={IconTraveler}
-              alt={`${props.data.travelers} Travelers`}
-              data={numberFormat(props.data.travelers)}
-              name="Travelers"
-              hasMarginRight
-            />
-            <IconHero
-              imageSrc={IconTreasure}
-              alt={`${props.data.treasures} treasures`}
-              data={numberFormat(props.data.treasures)}
-              name="treasures"
-              hasMarginRight
-            />
-            <IconHero
-              imageSrc={IconCities}
-              alt={`${props.data.cities} cities`}
-              data={numberFormat(props.data.cities)}
-              name="cities"
-            />
+    <Fade bottom>
+      <section className="container pt-4">
+        <div className="row align-items-center">
+          <div className="col-auto pr-5" style={{ width: 530 }}>
+            <h1 className="font-weight-bold line-height-1 mb-3">
+              Forget Busy Work, <br />
+              Start Next Vacation
+            </h1>
+            <p
+              className="mb-4 font-weight-light text-gray-500 w-75"
+              style={{ lineHeight: "170%" }}
+            >
+              We provide what you need to enjoy your holiday with family. Time
+              to make another memorable moments.
+            </p>
+            <Button
+              className="btn px-5"
+              hasShadow
+              isPrimary
+              onClick={showMostPicked}
+            >
+              Show Me Now
+            </Button>
+            <div className="row" style={{ marginTop: 80 }}>
+              <IconHero
+                imageSrc={IconTraveler}
+                alt={`${props.data.travelers} Travelers`}
+                data={numberFormat(props.data.travelers)}
+                name="Travelers"
+                hasMarginRight
+              />
+              <IconHero
+                imageSrc={IconTreasure}
+                alt={`${props.data.treasures} treasures`}
+                data={numberFormat(props.data.treasures)}
+                name="treasures"
+                hasMarginRight
+              />
+              <IconHero
+                imageSrc={IconCities}
+                alt={`${props.data.cities} cities`}
+                data={numberFormat(props.data.cities)}
+                name="cities"
+              />
+            </div>
+          </div>
+          <div className="col-6 pl-5">
+            <div style={{ width: 520, height: 410 }}>
+              <img
+                src={ImageHero}
+                alt="Room with Couches"
+                className="img-fluid position-absolute"
+                style={{ margin: "-30px 0 0 -30px", zIndex: 1 }}
+              />
+              <img
+                src={ImageHeroFrame}
+                alt="Room with Couches frame"
+                className="img-fluid position-absolute"
+                style={{ margin: "0 -15px -15px 0" }}
+              />
+            </div>
           </div>
         </div>
-        <div className="col-6 pl-5">
-          <div style={{ width: 520, height: 410 }}>
-            <img
-              src={ImageHero}
-              alt="Room with Couches"
-              className="img-fluid position-absolute"
-              style={{ margin: "-30px 0 0 -30px", zIndex: 1 }}
-            />
-            <img
-              src={ImageHeroFrame}
-              alt="Room with Couches frame"
-              className="img-fluid position-absolute"
-              style={{ margin: "0 -15px -15px 0" }}
-            />
-          </div>
-        </div>
-      </div>
-    </section>
+      </section>
+    </Fade>
   );
 }
