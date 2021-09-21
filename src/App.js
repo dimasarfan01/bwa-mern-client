588681cf8b6399fd75f55e96ad7a080a84d6dce4
dimasarfan01 <dimasarfanasyuro123@gmail.com>
2021-09-21 22:06:58 +0700

    added page checkout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import LandingPage from "pages/LandingPage";
+import DetailsPage from "pages/DetailsPage";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "assets/scss/style.scss";
+import Checkout from "pages/Checkout";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route path="/" component={LandingPage}></Route>
+        <Route exact path="/" component={LandingPage} />
+        <Route exact path="/properties/:id" component={DetailsPage} />
+        <Route exact path="/checkout" component={Checkout} />
       </Router>
     </div>
   );
